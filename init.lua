e1757588c2471af473099368bbf302c6f9eb85df
yoyoanbary <yoyoalon123>
2024-07-06 18:06:30 +0300

    got vimtex to work with sumatrapdf

--- init.lua
+++ init.lua
@@ -241,15 +241,17 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  -- {
-  --   'lervag/vimtex',
-  --   lazy = false, -- we don't want to lazy load VimTeX
-  --   -- tag = "v2.15", -- uncomment to pin to a specific release
-  --   init = function()
-  --     -- VimTeX configuration goes here, e.g.
-  --     vim.g.vimtex_view_method = 'zathura'
-  --   end,
-  -- },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'general'
+      vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+      vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+    end,
+  },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
